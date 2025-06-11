00da9a9b5283a7a9196d667e6d0319f2806b8fe0
Zaza Javakhishvili <zaza.javakhishvili@gmail.com>
2025-06-11 19:19:18 -0400

    fix

--- src/crypto.c
+++ src/crypto.c
@@ -132,7 +132,7 @@ entropy_check(void)
 }
 
 int
-none_stream (buffer_t *chipertext, cipher_ctx_t *chiper_ctx, size_t capacity) {Add commentMore actions
+none_stream (buffer_t *chipertext, cipher_ctx_t *chiper_ctx, size_t capacity) {
     (void) chipertext; (void) chiper_ctx; (void) capacity;
     return CRYPTO_OK;
 }
